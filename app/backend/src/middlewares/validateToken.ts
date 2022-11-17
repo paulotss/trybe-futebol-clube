727c1af4733b26750a0f238bf29e0b8986e5c81d
Paulo de Tarso <paulo.oinab@gmail.com>
2022-11-17 15:59:42 -0300

    Requisito 27 - correção

--- app/backend/src/middlewares/validateToken.ts
+++ app/backend/src/middlewares/validateToken.ts
@@ -1,11 +1,17 @@
 import { Request, Response, NextFunction } from 'express';
-import { jwtVerify } from '../utils/jwtToken';
+import * as jwt from 'jsonwebtoken';
+import 'dotenv';
 
 const validateToken = (req: Request, res: Response, next: NextFunction) => {
+  const secret = process.env.JWT_SECRET || 'jwt_secret';
   const { authorization } = req.headers;
   if (authorization) {
-    const result = jwtVerify(authorization);
-    if (typeof result !== 'string') return next();
+    try {
+      jwt.verify(authorization, secret);
+      return next()
+    } catch(err) {
+      return res.status(401).json({ message: 'Token must be a valid token' });
+    }
   }
   res.status(401).json({ message: 'Token must be a valid token' });
 };
